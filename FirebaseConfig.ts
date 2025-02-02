3c6c8482559fd7d65ef5d48cb9b083dbb47439b1
Raus17 <83296309+Raus17@users.noreply.github.com>
2025-02-03 01:53:41 +0530

    Update FirebaseConfig.ts

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANFZcl34LHenC4jj5Zw0ZCXyhs6rWOpKo",
-  authDomain: "taskbuddy-dd381.firebaseapp.com",
-  projectId: "taskbuddy-dd381",
-  storageBucket: "taskbuddy-dd381.firebasestorage.app",
-  messagingSenderId: "355751631967",
-  appId: "1:355751631967:web:ce01bd081b7f34476d4fea"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
